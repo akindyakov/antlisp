7c093fdb54c7a1f26825603d1dc03422d264d26b
akindyakov <akindyakov@gmail.com>
2018-02-04 22:24:02 +0300

    Add exception type

---
+++ antlisp/src/exception.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <exception>
+#include <string>
+#include <sstream>
+#include <iostream>
+
+
+namespace AntLisp {
+
+class Exception
+    : public std::exception
+{
+private:
+    std::string message_;
+
+public:
+    explicit Exception() = default;
+
+    explicit Exception(
+        std::string msg
+    )
+        : message_(std::move(msg))
+    {
+    }
+
+    virtual ~Exception() = default;
+
+    const char* what() const throw() override {
+        return message_.c_str();
+    }
+
+    const std::string& message() const noexcept{
+        return message_;
+    }
+
+    template<
+        typename T
+    >
+    Exception&& operator()(const T& value) {
+        std::ostringstream ostr;
+        ostr << value;
+        message_.append(
+            ostr.str()
+        );
+        return std::move(*this);
+    }
+};
+
+}  // namespace AntLisp
---
+++ antlisp/src/exception_ut.cpp
@@ -0,0 +1,19 @@
+#include "antlisp/src/exception.h"
+
+int main() {
+    auto ex = AntLisp::Exception()(
+        "abc"
+    )(
+        123
+    )(
+        "]"
+    );
+    auto expected = std::string("abc123]");
+    if (ex.message() != expected) {
+        std::cerr
+            << "Wrong value '" << ex.message()
+            << "', expected '" << expected << "'. ";
+        throw std::exception();
+    }
+    return 0;
+}
