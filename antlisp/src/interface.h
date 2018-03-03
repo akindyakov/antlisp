41a422359b1843bedab99f9d821db6f7562bbfd0
akindyakov <akindyakov@gmail.com>
2018-03-03 09:20:48 +0300

    create common lib for all libs in src

---
+++ antlisp/src/interface.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <antlisp/src/cell/cell.h>
+#include <antlisp/src/function/interface.h>
+#include <antlisp/src/function/stack_machine.h>
+
+#include <antlisp/src/parser/code_stream.h>
+#include <antlisp/src/parser/parentheses_parser.h>
+#include <antlisp/src/parser/parser.h>
