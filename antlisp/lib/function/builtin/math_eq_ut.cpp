7761798d233288270270435a88d6028f650b996a
akindyakov <akindyakov@gmail.com>
2018-03-18 18:15:46 +0300

    carry over equality unit test to separate file

---
+++ antlisp/lib/function/builtin/math_eq_ut.cpp
@@ -0,0 +1,126 @@
+#include "math.h"
+
+#include <antlisp/lib/test/ut.h>
+
+#include <iostream>
+
+
+void test_equality_int() {
+    auto eq = AntLisp::Builtin::Equality();
+    UT_ASSERT_EQUAL(
+        eq.instantCall(
+            AntLisp::Arguments{
+                AntLisp::Cell::integer(48),
+                AntLisp::Cell::integer(48),
+            }
+        ),
+        AntLisp::Cell::t()
+    );
+}
+
+void test_equality_int_neg() {
+    auto eq = AntLisp::Builtin::Equality();
+    UT_ASSERT_EQUAL(
+        eq.instantCall(
+            AntLisp::Arguments{
+                AntLisp::Cell::integer(155),
+                AntLisp::Cell::integer(-155),
+            }
+        ),
+        AntLisp::Cell::nil()
+    );
+}
+
+void test_equality_symbol() {
+    auto eq = AntLisp::Builtin::Equality();
+    UT_ASSERT_EQUAL(
+        eq.instantCall(
+            AntLisp::Arguments{
+                AntLisp::Cell::symbol('t'),
+                AntLisp::Cell::symbol('t'),
+            }
+        ),
+        AntLisp::Cell::t()
+    );
+}
+
+void test_equality_symbol_nil() {
+    auto eq = AntLisp::Builtin::Equality();
+    UT_ASSERT_EQUAL(
+        eq.instantCall(
+            AntLisp::Arguments{
+                AntLisp::Cell::symbol('a'),
+                AntLisp::Cell::symbol('A'),
+            }
+        ),
+        AntLisp::Cell::nil()
+    );
+}
+
+void test_equality_string() {
+    auto eq = AntLisp::Builtin::Equality();
+    UT_ASSERT_EQUAL(
+        eq.instantCall(
+            AntLisp::Arguments{
+                AntLisp::Cell::string("keymaster"),
+                AntLisp::Cell::string("keymaster"),
+            }
+        ),
+        AntLisp::Cell::t()
+    );
+}
+
+void test_equality_string_nil() {
+    auto eq = AntLisp::Builtin::Equality();
+    UT_ASSERT_EQUAL(
+        eq.instantCall(
+            AntLisp::Arguments{
+                AntLisp::Cell::string("key master"),
+                AntLisp::Cell::string("keymaster"),
+            }
+        ),
+        AntLisp::Cell::nil()
+    );
+}
+
+void test_equality_multi_symbol() {
+    auto eq = AntLisp::Builtin::Equality();
+    UT_ASSERT_EQUAL(
+        eq.instantCall(
+            AntLisp::Arguments{
+                AntLisp::Cell::symbol('t'),
+                AntLisp::Cell::symbol('t'),
+                AntLisp::Cell::symbol('t'),
+                AntLisp::Cell::symbol('t'),
+                AntLisp::Cell::symbol('t'),
+            }
+        ),
+        AntLisp::Cell::t()
+    );
+}
+
+void test_equality_multi_symbol_nil() {
+    auto eq = AntLisp::Builtin::Equality();
+    UT_ASSERT_EQUAL(
+        eq.instantCall(
+            AntLisp::Arguments{
+                AntLisp::Cell::symbol('u'),
+                AntLisp::Cell::symbol('u'),
+                AntLisp::Cell::symbol('*'),
+                AntLisp::Cell::symbol('u'),
+            }
+        ),
+        AntLisp::Cell::nil()
+    );
+}
+
+UT_LIST(
+    RUN_TEST(test_equality_int);
+    RUN_TEST(test_equality_int_neg);
+    RUN_TEST(test_equality_symbol);
+    RUN_TEST(test_equality_symbol_nil);
+    RUN_TEST(test_equality_string);
+    RUN_TEST(test_equality_string_nil);
+    RUN_TEST(test_equality_multi_symbol);
+    RUN_TEST(test_equality_multi_symbol_nil);
+);
--- antlisp/lib/function/builtin/math_ut.cpp
+++ antlisp/lib/function/builtin/math_ut.cpp
@@ -5,87 +5,6 @@
 #include <iostream>
 
 
-void test_equality() {
-    auto eq = AntLisp::Builtin::Equality();
-    UT_ASSERT_EQUAL(
-        eq.instantCall(
-            AntLisp::Arguments{
-                AntLisp::Cell::integer(48),
-                AntLisp::Cell::integer(48),
-            }
-        ),
-        AntLisp::Cell::t()
-    );
-    UT_ASSERT_EQUAL(
-        eq.instantCall(
-            AntLisp::Arguments{
-                AntLisp::Cell::integer(155),
-                AntLisp::Cell::integer(-155),
-            }
-        ),
-        AntLisp::Cell::nil()
-    );
-    UT_ASSERT_EQUAL(
-        eq.instantCall(
-            AntLisp::Arguments{
-                AntLisp::Cell::symbol('t'),
-                AntLisp::Cell::symbol('t'),
-            }
-        ),
-        AntLisp::Cell::t()
-    );
-    UT_ASSERT_EQUAL(
-        eq.instantCall(
-            AntLisp::Arguments{
-                AntLisp::Cell::symbol('a'),
-                AntLisp::Cell::symbol('A'),
-            }
-        ),
-        AntLisp::Cell::nil()
-    );
-    UT_ASSERT_EQUAL(
-        eq.instantCall(
-            AntLisp::Arguments{
-                AntLisp::Cell::string("keymaster"),
-                AntLisp::Cell::string("keymaster"),
-            }
-        ),
-        AntLisp::Cell::t()
-    );
-    UT_ASSERT_EQUAL(
-        eq.instantCall(
-            AntLisp::Arguments{
-                AntLisp::Cell::string("key master"),
-                AntLisp::Cell::string("keymaster"),
-            }
-        ),
-        AntLisp::Cell::nil()
-    );
-    UT_ASSERT_EQUAL(
-        eq.instantCall(
-            AntLisp::Arguments{
-                AntLisp::Cell::symbol('t'),
-                AntLisp::Cell::symbol('t'),
-                AntLisp::Cell::symbol('t'),
-                AntLisp::Cell::symbol('t'),
-                AntLisp::Cell::symbol('t'),
-            }
-        ),
-        AntLisp::Cell::t()
-    );
-    UT_ASSERT_EQUAL(
-        eq.instantCall(
-            AntLisp::Arguments{
-                AntLisp::Cell::symbol('u'),
-                AntLisp::Cell::symbol('u'),
-                AntLisp::Cell::symbol('*'),
-                AntLisp::Cell::symbol('u'),
-            }
-        ),
-        AntLisp::Cell::nil()
-    );
-}
-
 void test_allMathBuiltinFunctions() {
     auto space = AntLisp::Namespace{};
     AntLisp::Builtin::allMathFunctions(space);
@@ -132,6 +51,5 @@ void test_allMathBuiltinFunctions() {
 }
 
 UT_LIST(
-    RUN_TEST(test_equality);
     RUN_TEST(test_allMathBuiltinFunctions);
 );
